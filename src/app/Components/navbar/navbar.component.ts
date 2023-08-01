630d853c876c4ec6fa7721d89eb46de48d9f00e5
Nada Sabry <Nada_201900894@fci.helwan.edu.eg>
2023-08-01 10:47:51 +0300

    finish navbar

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
-
+export class NavbarComponent implements OnInit {
+  mobileScreen = false;
+  toggle = false;
+  ngOnInit(): void {
+    (window.innerWidth < 600)?this.mobileScreen = true:this.mobileScreen = false;
+  }
+  
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    if (window.innerWidth < 600) {
+      this.mobileScreen = true;
+    }
+    else{
+      this.mobileScreen = false;
+    }
+  }
+  toggleMenu() {
+    if (this.toggle == true)
+      this.toggle = false;
+    else
+      this.toggle = true;
+    console.log("click button ",  this.toggle);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
+import { ServicesComponent } from './Components/services/services.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavbarComponent
+    NavbarComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
